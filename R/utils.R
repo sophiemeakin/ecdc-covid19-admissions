dd16c08f11fb2d9c4c63fe36106f6987d3675a61
Sophie Meakin <sophiemeakin@outlook.com>
2021-11-08 16:17:16 +0000

    Update get_forecast_ids
    * return tibble with location (id) and week that admissions were last reported

--- R/utils.R
+++ R/utils.R
@@ -2,13 +2,26 @@
 # Get vector of country IDs that we will make admissions forecasts for
 get_forecast_ids <- function(dat, forecast_date) {
   
-  out <- dat %>%
-    filter(week >= forecast_date - 9*7,
+  # Get last reported week
+  last_rep <- raw_dat %>%
+    filter(!is.na(adm),
            week < forecast_date) %>%
     group_by(location) %>%
-    summarise(all_adm = sum(adm)) %>% 
+    filter(week == max(week)) %>%
+    ungroup() %>%
+    filter(week >= forecast_date - 8*7) %>%
+    select(location, last_rep = week)
+  
+  # Remove locations missing data in the last 8 weeks
+  out <- raw_dat %>%
+    left_join(last_rep, by = "location") %>%
+    filter(week >= last_rep - 8*7,
+           week < last_rep) %>%
+    group_by(location, last_rep) %>%
+    summarise(all_adm = sum(adm),
+              .groups = "drop") %>% 
     filter(!is.na(all_adm)) %>%
-    pull(location)
+    select(id = location, last_rep)
   
   return(out)
   
